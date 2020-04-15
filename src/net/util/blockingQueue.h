d7f3566ae1b3cc325e36bb5119c13b8a8d84b47d
Jiaju Du <i@dujiaju.me>
2020-04-15 03:42:41 -0700

    delete blocking queue

--- src/net/util/blockingQueue.h
+++ src/net/util/blockingQueue.h
@@ -3,33 +3,6 @@
 #include <condition_variable>
 #include <mutex>
 #include <queue>
-template <class T> class BlockingQueue {
-public:
-    BlockingQueue(typename queue<T>::size_type n) : capacity(n) {
-    }
-    typename queue<T>::size_type capacity;
-    mutex lock;
-    condition_variable condition_push;
-    condition_variable condition_pop;
-    queue<T> q;
-    void push(const T &value) {
-        unique_lock<mutex> ulock(lock);
-        while (q.size() >= capacity) {
-            condition_push.wait(ulock);
-        }
-        q.push(value);
-        condition_pop.notify_one();
-    }
-    T pop() {
-        unique_lock<mutex> ulock(lock);
-        while (q.size() <= 0) {
-            condition_pop.wait(ulock);
-        }
-        auto ret = q.front();
-        q.pop();
-        return ret;
-    }
-};
 template <class T> class Queue {
 public:
     Queue() {
